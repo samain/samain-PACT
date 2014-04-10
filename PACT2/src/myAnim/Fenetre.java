32f2521ca30270f84395d71d9f69c2a8f1779714
Pauline Rabis <pauline.rabis@telecom-paristech.fr>
2014-04-10 19:16:27 +0200

    Travail du 10/04/2014 : création des classes PanneauAmbiances et Cellule, utilisation du GridBagLayout pour séparer la fenêtre en containers Ambiances, Thèmes et Livres. L'interface est encore en travail! (Il reste à positionner un peu mieux les ambiances, gérer le container Thèmes et ajouter les animations dans le container Livres, et ajouter des Listeners partout)

--- PACT2/src/myAnim/Fenetre.java
+++ PACT2/src/myAnim/Fenetre.java
@@ -1,84 +1,86 @@
 package myAnim;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class Fenetre extends JFrame {
-	private float x1;
-	private float y1;
-	private float x2;
-	private float y2;
-	private JLabel label = new JLabel("Livres");
-	private AnimModel animModel = new AnimModel(x1,y1,x2,y2);
-	private PanneauLivres pan = new PanneauLivres();
-	private PanneauAnimation anim = new PanneauAnimation();
+	private JButton bouton = new JButton("Réglages");
+	
 	
 	public Fenetre() {
-		this.setTitle("Immersive reading");
-		this.setSize(900, 700);
+		this.setTitle("ImmersiveReading");
+		this.setSize(1200, 900);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
-		pan.add(anim);
-		this.setContentPane(pan);
-		this.add(label);
+		// On crée nos différents conteneurs de couleur différente
+		JPanel cell1 = new PanneauAmbiances();
+		//cell1.setBackground(Color.YELLOW);
+		cell1.setPreferredSize(new Dimension(300, 900));
+		
+		JPanel cell2 = new JPanel();
+		cell2.setBackground(Color.red);
+		cell2.setPreferredSize(new Dimension(600, 900));
+
+		JPanel cell4 = new PanneauLivres();
+		// cell4.setBackground(Color.black);
+		cell4.setPreferredSize(new Dimension(300, 900));
+
+		// Le conteneur principal
+		JPanel content = new JPanel();
+		content.setPreferredSize(new Dimension(1200, 900));
+		content.setBackground(Color.WHITE);
+		// On définit le layout manager
+		content.setLayout(new GridBagLayout());
+
+		GridBagConstraints gbc = new GridBagConstraints(0, 0,
+				GridBagConstraints.REMAINDER, GridBagConstraints.REMAINDER, 1,
+				1, GridBagConstraints.FIRST_LINE_START,
+				GridBagConstraints.BOTH, new Insets(1, 1, 0, 0), 0, 0);
+
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+
+		gbc.gridheight = 2;
+		gbc.gridwidth = 1;
+
+		content.add(cell1, gbc);
+
+		gbc.gridx = 1;
+		gbc.gridheight = 2;
+
+		content.add(cell2, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.gridwidth = 2;
+		gbc.gridheight = 1;
+		// Celle-ci indique que la cellule se réplique de façon verticale
+
+		content.add(cell4, gbc);
+
+		this.setContentPane(content);
+		content.add(bouton);
 		this.setVisible(true);
-		//go();
-	}
-
-	/*private void go() {
-		// Les coordonnées de départ de notre rond
-		int x = pan.getPosX(), y = pan.getPosY();
-		// Le booléen pour savoir si l'on recule ou non sur l'axe x
-		boolean backX = false;
-		// Le booléen pour savoir si l'on recule ou non sur l'axe y
-		boolean backY = false;
-
-		// Dans cet exemple, j'utilise une boucle while
-		// Vous verrez qu'elle fonctionne très bien
-		while (true) {
-			// Si la coordonnée x est inférieure à 1, on avance
-			if (x < 1)
-				backX = false;
-
-			// Si la coordonnée x est supérieure à la taille du Panneau moins la
-			// taille du rond, on recule
-			if (x > pan.getWidth() - 50)
-				backX = true;
-
-			// Idem pour l'axe y
-			if (y < 1)
-				backY = false;
-			if (y > pan.getHeight() - 50)
-				backY = true;
-
-			// Si on avance, on incrémente la coordonnée
-			// backX est un booléen, donc !backX revient à écrire
-			// if (backX == false)
-			if (!backX)
-				pan.setPosX(++x);
-
-			// Sinon, on décrémente
-			else
-				pan.setPosX(--x);
-
-			// Idem pour l'axe Y
-			if (!backY)
-				pan.setPosY(++y);
-			else
-				pan.setPosY(--y);
-
-			// On redessine notre Panneau
-			pan.repaint();
-
-			// Comme on dit : la pause s'impose ! Ici, trois millièmes de
-			// seconde
-			try {
-				Thread.sleep(3);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+		bouton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				ZDialog zd = new ZDialog(null, "Réglages Immersive Reading",
+						true);
+				ZDialogInfo zInfo = zd.showZDialog();
+				JOptionPane jop = new JOptionPane();
+				jop.showMessageDialog(null, zInfo.toString(), "Informations personnage", jop.INFORMATION_MESSAGE);
 			}
-		}
-	}*/
+		});
+	}
 }
---
+++ PACT2/src/myAnim/PanneauAmbiances.java
@@ -0,0 +1,123 @@
+package myAnim;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+public class PanneauAmbiances extends JPanel {
+
+	private JRadioButton jr1 = new JRadioButton("Relaxation");
+	private JRadioButton jr2 = new JRadioButton("Aventure");
+	private JRadioButton jr3 = new JRadioButton("Sommeil");
+	
+	public PanneauAmbiances() {
+		this.setLayout(new GridBagLayout());
+
+		this.setPreferredSize(new Dimension(600, 500));
+
+		JPanel cell1 = new Cellule(new File("/Users/paulinerabis/Documents/relaxation.jpg"));
+		//cell1.setBackground(Color.YELLOW);
+		cell1.add(jr1);
+		
+		cell1.setPreferredSize(new Dimension(60, 40));
+		JPanel cell2 = new Cellule(new File("/Users/paulinerabis/Documents/aventure.jpg"));
+		//cell2.setBackground(Color.red);
+		cell2.add(jr2);
+		cell2.setPreferredSize(new Dimension(60, 40));
+		JPanel cell3 = new Cellule(new File("/Users/paulinerabis/Documents/nuit.jpg"));
+		cell3.setBackground(Color.green);
+		cell3.setPreferredSize(new Dimension(60, 40));
+		cell3.add(jr3);
+		JPanel cell4 = new JPanel();
+		cell4.setBackground(Color.black);
+		cell4.setPreferredSize(new Dimension(60, 40));
+		JPanel cell5 = new JPanel();
+		cell5.setBackground(Color.cyan);
+		cell5.setPreferredSize(new Dimension(60, 40));
+		JPanel cell6 = new JPanel();
+		cell6.setBackground(Color.BLUE);
+		cell6.setPreferredSize(new Dimension(60, 40));
+		JPanel cell7 = new JPanel();
+		cell7.setBackground(Color.orange);
+		cell7.setPreferredSize(new Dimension(60, 40));
+		JPanel cell8 = new JPanel();
+		cell8.setBackground(Color.DARK_GRAY);
+		cell8.setPreferredSize(new Dimension(60, 40));
+		
+
+		// L'objet servant à positionner les composants
+		GridBagConstraints gbc = new GridBagConstraints(0, 0,
+				GridBagConstraints.REMAINDER, GridBagConstraints.REMAINDER, 1,
+				1, GridBagConstraints.FIRST_LINE_START,
+				GridBagConstraints.BOTH, new Insets(1, 1, 0, 0), 0, 0);
+
+		// On positionne la case de départ du composant
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		// La taille en hauteur et en largeur
+		gbc.gridheight = 1;
+		gbc.gridwidth = 1;
+		this.add(cell1, gbc);
+		// ---------------------------------------------
+		gbc.gridx = 1;
+		this.add(cell2, gbc);
+		// ---------------------------------------------
+		gbc.gridx = 2;
+		this.add(cell3, gbc);
+		// ---------------------------------------------
+		// Cette instruction informe le layout que c'est une fin de ligne
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.gridx = 3;
+		this.add(cell4, gbc);
+		// ---------------------------------------------
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 2;
+		// Celle-ci indique que la cellule se réplique de façon verticale
+		gbc.fill = GridBagConstraints.VERTICAL;
+		this.add(cell5, gbc);
+		// ---------------------------------------------
+		gbc.gridx = 1;
+		gbc.gridheight = 1;
+		// Celle-ci indique que la cellule se réplique de façon horizontale
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		this.add(cell6, gbc);
+		// ---------------------------------------------
+		gbc.gridx = 1;
+		gbc.gridy = 2;
+		gbc.gridwidth = 2;
+		this.add(cell7, gbc);
+		// ---------------------------------------------
+		gbc.gridx = 3;
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		this.add(cell8, gbc);
+		
+		
+	}
+	protected void paintComponent(Graphics g) {
+
+		try {
+			Image img = ImageIO.read(new File("/Users/paulinerabis/Documents/cubes.jpg"));
+
+			g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+
+}
